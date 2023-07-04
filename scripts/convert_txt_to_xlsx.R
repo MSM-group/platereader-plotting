779ecd024a63aa7833162fcb0f2665c2a8434a14
DianaSchweizer <diana.schweizer@eawag.ch>
2023-07-04 19:19:13 +0200

    convert_txt_to_xlsx_with_comments

--- scripts/convert_txt_to_xlsx.R
+++ scripts/convert_txt_to_xlsx.R
@@ -4,10 +4,14 @@ install.packages("writexl")
 library(readr)
 library(writexl)
 
-txt_file <- "C:\\Users\\schweidi\\Documents\\GitHub\\platereader-plotting\\data\\hplctesttxt\\Mutation_LB_S146_alignment.txt"
+#insert file name
+
+txt_file <- "C:\\Users\\schweidi\\Documents\\GitHub\\platereader-plotting\\data\\hplc_txt\\filename.txt"
 
 data <- read_delim(txt_file, delim = "\t")
 
-xlsx_file <- "C:\\Users\\schweidi\\Documents\\GitHub\\platereader-plotting\\data\\hplctest\\Mutation_LB_S170_alignment.xlsx"
+#insert file name
+
+xlsx_file <- "C:\\Users\\schweidi\\Documents\\GitHub\\platereader-plotting\\data\\hplc_xlsx\\filename.xlsx"
 
 write_xlsx(data, path = xlsx_file)
